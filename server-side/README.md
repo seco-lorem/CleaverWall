5124ea6e4116a0194224f3348e33f9ead456c011
Yekta Seçkin Satır <seckin.satir@ug.bilkent.edu.tr>
2023-03-24 10:10:33 +0300

    initiated the ubuntu server

--- server-side/README.md
+++ server-side/README.md
@@ -1,12 +1,14 @@
 # CleaverWall Server Side
 
-Server Side Application of CleaverWall
+Server Side Applications of CleaverWall. \
+There are two servers: one main Django server, one fastAPI server that specificly runs on Ubuntu 20.04.
+
+## Main Server
+in ./cleaverwall/
 
 ### To start
-
 ```
 pip install -r "requirements.txt"
-cd cleaverwall
 python manage.py runserver
 ```
 
@@ -14,4 +16,17 @@ python manage.py runserver
 ```
 python manage.py makemigrations
 python manage.py migrate
+```
+
+## Ubuntu Server
+
+in ./ubuntu-server/
+
+### To start
+
+First, on an Ubuntu 20.04 machine, set up and run Cuckoo Sandbox...\
+Then
+```
+pip install -r "requirements.txt"
+uvicorn main:app --reload
 ```
---
+++ server-side/ubuntu-server/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import sqlite3
+import asyncio
+
+app = FastAPI()
+conn = sqlite3.connect('example.db', check_same_thread=False)
+conn.execute('CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT, num INTEGER, data JSON)')
+
+@app.get("/")
+async def get_num(num: int, data: dict):
+    conn.execute('INSERT INTO data (num, data) VALUES (?, ?)', (num, data))
+    conn.commit()
+
+    # Call the expensive function in a separate task
+    asyncio.create_task(do_expensive_task())
+
+    return {"status": "OK"}
+
+@app.get("/{num}")
+async def get_data(num: int):
+    cursor = conn.execute('SELECT data FROM data WHERE num = ?', (num,))
+    result = cursor.fetchone()
+
+    if result is None:
+        return {"status": "not found"}
+
+    return result[0]
+
+async def do_expensive_task():
+    # simulate an expensive task by sleeping for a few seconds
+    await asyncio.sleep(5)
+    print("expensive task completed")
+
